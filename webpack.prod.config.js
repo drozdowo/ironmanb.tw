be600d794d8421db3414f30ced65abe472047f91
Oliver Drozdowski <drozdowo@uwindsor.ca>
2018-12-21 21:15:11 -0500

    added production config

---
+++ webpack.prod.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/react']
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader']
+          }
+        ],
+      }
+};
